dc93761656e99221870acefff572a876b1c3f3d0
Usman Safder <143291644+usmansafdarktk@users.noreply.github.com>
2025-11-25 13:26:07 +0500

    Refactor basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
+const basePath = "/Librai-Open-index";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/Librai-Open-index",
-  assetPrefix: "/Librai-Open-index",
+  basePath: basePath,
+  assetPrefix: basePath,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
